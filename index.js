885497d935f27b1a35155eee35f51bc3a226bc4c
Mitya <mitro_fan@inbox.ru>
2022-10-23 00:12:57 +0300

    bot start

---
+++ index.js
@@ -0,0 +1,68 @@
+const TelegramApi = require('node-telegram-bot-api')
+const { gameOptions, againOptions } = require('./options')
+
+const token = '556977614:AAGanGW979JfigtcHSIrl14NmUoy4VX27yo'
+
+const bot = new TelegramApi(token, { polling: true })
+
+const chats = {}
+
+
+const startGame = async (chatId) => {
+    await bot.sendMessage(chatId, 'Сейчас я загадаю цифру от 0 до 9')
+    const randomNumber = Math.floor(Math.random() * 10)
+    chats[chatId] = randomNumber
+    await bot.sendMessage(chatId, 'Отгадывай', gameOptions)
+}
+
+
+
+const start = () => {
+    bot.setMyCommands([
+        { command: '/start', description: 'Начальное приветствие' },
+        { command: '/info', description: 'Твоё имя' },
+        { command: '/game', description: 'Игра' }
+    ])
+
+    bot.on('message', async msg => {
+        const text = msg.text
+        const chatId = msg.from.id
+
+        if (text === '/again') {
+            return startGame(chatId)
+        }
+
+        if (text === '/start') {
+            await bot.sendSticker(chatId, 'https://tlgrm.eu/_/stickers/4dd/300/4dd300fd-0a89-3f3d-ac53-8ec93976495e/256/3.webp')
+            return bot.sendMessage(chatId, 'Кваха')
+        }
+
+        if (text === '/info') {
+
+            return bot.sendMessage(chatId, `Тебя зовут ${msg.from.first_name}`)
+        }
+
+        if (text === '/game') {
+            return startGame(chatId)
+        }
+
+        return bot.sendMessage(chatId, 'Я тебя не понимаю')
+
+    })
+
+    bot.on('callback_query', async msg => {
+        const data = msg.data
+        const chatId = msg.message.chat.id
+        if (data === '/again') {
+            return startGame(chatId)
+        }
+
+        if (data === chats[chatId]) {
+            return await bot.sendMessage(chatId, `Поздравляю, ты отгадал ${chats[chatId]}`, againOptions)
+        } else {
+            return bot.sendMessage(chatId, `К сожалению, ты не отгадал бот выбрал цифру ${chats[chatId]}`, againOptions)
+        }
+    })
+}
+
+start()
